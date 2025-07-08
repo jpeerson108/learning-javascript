edc28dd90bfaaf4ac5e0bc85d0bc7f736c23e7fd
Jaret Peerson <jaretpeerson@gmail.com>
2025-07-08 07:07:04 -0400

    Data Attributes

--- script.js
+++ script.js
@@ -457,3 +457,16 @@ console.log(hobbies)
     console.log("success")
   })
 }
+
+// Simple Button Counter Application
+{
+  const buttons = document.querySelectorAll("button")
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const currentClicks = parseInt(button.dataset.clicks)
+
+      button.dataset.clicks = currentClicks + 1
+    })
+  })
+}
