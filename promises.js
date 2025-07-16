798a5a0d57d61570dcf7f61c418f581b075c9d0e
Jaret Peerson <jaretpeerson@gmail.com>
2025-07-15 20:52:33 -0400

    Promises Initial Commit

---
+++ promises.js
@@ -0,0 +1,35 @@
+// Example 1
+const promise = new Promise((resolve, reject) => {
+  const sum = 1 + 1
+  if (sum === 2) {
+    resolve("success")
+  } else {
+    reject("error")
+  }
+})
+
+promise
+  .then((message) => {
+    console.log(message)
+  })
+  .catch(message => {
+    console.error(message)
+  }
+
+
+// Example 2
+  setTimeout(() => {
+    console.log("here")
+  }, 250)
+
+    // Above converted to promise
+
+setTimeoutPromise(250).then(() => {
+    console.log('here')
+})
+
+function setTimeoutPromise(duration) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, duration)
+    })
+}
