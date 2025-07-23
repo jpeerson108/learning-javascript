ed848e72cecb018d4bf8f18c3b32c6c088e61977
Jaret Peerson <jaretpeerson@gmail.com>
2025-07-23 09:56:36 -0400

    Fetch & Map API Data

--- Advanced DOM/AdvancedDOM.js
+++ Advanced DOM/AdvancedDOM.js
@@ -10,3 +10,14 @@ fetch("https://jsonplaceholder.typicode.com/users")
     // Log the name of each of the users
     console.log(data.map((user) => user.name))
   })
+
+// Explained
+// fetch gets data from the given URL
+// The first .then((response) => response.json()):
+//// response is the raw response
+//// .json() parses the body into usable JSON data
+// The second .then((data) => { ... }):
+//// data is now an array of users
+//// .map((user) => user.name) loops through the users and creates a new array of just the name values
+//// user is just the name for each user in the loop
+// console.log(...) prints the new array of names
